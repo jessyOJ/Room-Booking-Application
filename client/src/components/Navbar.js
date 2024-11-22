51d259ef6b4c945e8b65c3d0b3ab270ed14e68a7
jessyOJ <jessicachioma23@outlook.com>
2024-11-22 21:30:07 +0100

    front end review

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,62 +1,69 @@
 import React from "react";
-import {Link} from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom';
+
 function Navbar() {
-  function logout(){
-    localStorage.removeItem('currentUser')
-    window.location.href='/'
+  const navigate = useNavigate();
+
+  function logout() {
+    localStorage.removeItem('currentUser');
+    navigate('/');
   }
+
+  const user = JSON.parse(localStorage.getItem('currentUser'));
   
-  const user =JSON.parse(localStorage.getItem('currentUser'))
   return (
     <div>
-    <nav class="navbar navbar-expand-lg ">
-      <div class="container-fluid">
-        <Link class="navbar-brand" to="/home">
-          Jessy Room Booking Service
-        </Link>
-        <button
-          class="navbar-toggler"
-          type="button"
-          data-bs-toggle="collapse"
-          data-bs-target="#navbarNav"
-          aria-controls="navbarNav"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
-        >
-          <span class="navbar-toggler-icon" ><i class="fa fa-hamburger"></i></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarNav">
-          <ul class="navbar-nav ms-auto mr-3" >
-            {user ? (<>
-              <div class="dropdown">
-  <Link class="btn btn-secondary dropdown-toggle"style={{ color: "MistyRose",backgroundColor:'black' }}  to="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-  <i class="fa fa-user"></i> {user.name}
-  </Link>
-
-  <ul class="dropdown-menu">
-    <li><Link class="dropdown-item" to="/profile">Profile</Link></li>
-    {user.isAdmin &&(<li><Link class="dropdown-item" to="/admins">Admin</Link></li>)}
-    <li><Link class="dropdown-item" to="/" onClick={logout}>Logout</Link></li>
- 
-  </ul>
-</div></>):(<>
-            <li class="nav-item">
-              <Link class="nav-link" aria-current="page" to="/register">
-                Register
-              </Link>
-            </li>
-            <li class="nav-item">
-              <Link class="nav-link" to="/login">
-                Login
-              </Link>
-            </li>
-           </>)}
-          </ul>
+      <nav className="navbar navbar-expand-lg">
+        <div className="container-fluid">
+          <Link className="navbar-brand" to="/home">
+            Jessy Room Booking Service
+          </Link>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarNav"
+            aria-controls="navbarNav"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"><i className="fa fa-hamburger"></i></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarNav">
+            <ul className="navbar-nav ms-auto mr-3">
+              {user ? (
+                <>
+                  <div className="dropdown">
+                    <Link className="btn btn-secondary dropdown-toggle" style={{ color: "MistyRose", backgroundColor: 'black' }} to="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                      <i className="fa fa-user"></i> {user.name}
+                    </Link>
+                    <ul className="dropdown-menu">
+                      <li><Link className="dropdown-item" to="/profile">Profile</Link></li>
+                      {user.isAdmin && (<li><Link className="dropdown-item" to="/admins">Admin</Link></li>)}
+                      <li><Link className="dropdown-item" to="/" onClick={logout}>Logout</Link></li>
+                    </ul>
+                  </div>
+                </>
+              ) : (
+                <>
+                  <li className="nav-item">
+                    <Link className="nav-link" aria-current="page" to="/register">
+                      Register
+                    </Link>
+                  </li>
+                  <li className="nav-item">
+                    <Link className="nav-link" to="/login">
+                      Login
+                    </Link>
+                  </li>
+                </>
+              )}
+            </ul>
+          </div>
         </div>
-      </div>
-    </nav>
-  </div>
-  )
+      </nav>
+    </div>
+  );
 }
 
 export default Navbar;
